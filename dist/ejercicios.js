c09502ae3cb3a013e101a7d03360ea7458117d1d
Erickjllo <erickjllo07@gmail.com>
2026-08-21 14:44:34 -0500

    1. Crear un array con 5 nombres de estudiantes y mostrar cada nombre en consola usando un ciclo.

--- dist/ejercicios.js
+++ dist/ejercicios.js
@@ -1,4 +1,14 @@
 "use strict";
+// Ejercicio 1
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hola TypeScript");
+let estudiantes = [
+    "Ana",
+    "Carlos",
+    "María",
+    "Juan",
+    "Sofía"
+];
+for (let i = 0; i < estudiantes.length; i++) {
+    console.log(estudiantes[i]);
+}
 //# sourceMappingURL=ejercicios.js.map
--- src/ejercicios.ts
+++ src/ejercicios.ts
@@ -1,1 +1,14 @@
-console.log("Hola TypeScript");
+// Ejercicio 1
+
+let estudiantes: string[] = [
+    "Ana",
+    "Carlos",
+    "María",
+    "Juan",
+    "Sofía"
+];
+
+for (let i = 0; i < estudiantes.length; i++) {
+    console.log(estudiantes[i]);
+}
+
